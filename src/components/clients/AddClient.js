426177b1cb9ff3d5031f4f5338f8b7deb64f1b74
shuwh <shuwh317@gmail.com>
2019-03-06 12:25:00 -0800

    AddClient Component added
    1. add new client info to firestore
    2. push the page to root html when submitted

---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+// import { compose } from "redux";
+// import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+
+export class AddClient extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    balance: ""
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { firestore, history } = this.props;
+    const newClient = { ...this.state };
+
+    // If the balance is null, set it to 0
+    if (newClient.balance === "") {
+      newClient.balance = 0;
+    }
+
+    firestore
+      .add({ collection: "clients" }, newClient)
+      .then(() => history.push("/"));
+  };
+
+  onChange = e => this.setState({ [e.target.id]: e.target.value });
+
+  render() {
+    const { firstName, lastName, email, phone, balance } = this.state;
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn btn-link text-secondary">
+              <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">Add Contact</div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstName"
+                  minLength="2"
+                  required
+                  onChange={this.onChange}
+                  value={firstName}
+                  placeholder={"Enter First Name..."}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  minLength="2"
+                  required
+                  onChange={this.onChange}
+                  value={lastName}
+                  placeholder={"Enter Last Name..."}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  onChange={this.onChange}
+                  value={email}
+                  placeholder={"Enter Email..."}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phone"
+                  onChange={this.onChange}
+                  value={phone}
+                  placeholder={"Enter Phone..."}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="balance">Balance</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="balance"
+                  onChange={this.onChange}
+                  value={balance}
+                  placeholder={"Enter Balance..."}
+                />
+              </div>
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-primary btn-block"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default firestoreConnect()(AddClient);
